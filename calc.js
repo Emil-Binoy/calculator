40bf15ded477f33999e5946b2e1371d82c282e19
Emil Binoy <emilbinoy06@gmail.com>
2024-12-18 23:06:02 +0530

    working condition

--- calc.js
+++ calc.js
@@ -1,13 +1,29 @@
-// Get the toggle button and the menu
+
 const toggleButton = document.querySelector('[data-collapse-toggle="navbar-cta"]');
 const menu = document.getElementById("navbar-cta");
 
-// Add event listener to the hamburger button to toggle the menu
+
 toggleButton.addEventListener("click", function () {
-    // Toggle the 'hidden' class on the menu to show/hide it
+    
     menu.classList.toggle("hidden");
 
-    // Toggle aria-expanded attribute for better accessibility
+    
     const isExpanded = menu.classList.contains("hidden");
     toggleButton.setAttribute("aria-expanded", !isExpanded);
 });
+function inp(num){
+    document.getElementById("inpbox").value +=num;
+}
+function clr(){
+    document.getElementById("inpbox").value=" ";
+}
+function calc(){
+    const display =document.getElementById("inpbox");
+    try{
+        display.value=eval(display.value);
+    }
+    catch(e){
+        display.value= "Error";
+    }
+
+}
